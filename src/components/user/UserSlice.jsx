a890dc0be85fc8030f001626b4490f66a49747b2
Charles <charles.jouenne@gmail.com>
2024-09-16 20:58:07 +0200

    Gestion des erreurs

--- src/components/user/UserSlice.jsx
+++ src/components/user/UserSlice.jsx
@@ -9,33 +9,48 @@ const initialState = {
     id: '',
     createdAt: '',
     updatedAt: '',
+    error: null,
 }
 
 // POST request to API to retrieve user profile information
 export const fetchUserData = createAsyncThunk(
     'user/getUserData',
-    async (token) => {
-        const res = await axios({
-            method: 'post',
-            url: 'http://localhost:3001/api/v1/user/profile',
-            headers: { Authorization: `Bearer ${token}` },
-        })
-        return res.data.body
+    async (token, { rejectWithValue }) => {
+        try {
+            const res = await axios({
+                method: 'post',
+                url: 'http://localhost:3001/api/v1/user/profile',
+                headers: { Authorization: `Bearer ${token}` },
+            })
+            return res.data.body
+        } catch (error) {
+            // Return a custom error message if network error
+            if (!error.response) {
+                return rejectWithValue('ERR_NETWORK')
+            }
+            return rejectWithValue(error.response.data.message)
+        }
     }
 )
 
 // PUT request is sent to the API to update the user profile with new data
 export const updateUserData = createAsyncThunk(
     'user/updateUserData',
-    async (data) => {
-        const res = await axios({
-            method: 'put',
-            url: 'http://localhost:3001/api/v1/user/profile',
-            headers: { Authorization: `Bearer ${data.token}` },
-            data: data.userNames,
-        })
-
-        return res.data.body
+    async (data, { rejectWithValue }) => {
+        try {
+            const res = await axios({
+                method: 'put',
+                url: 'http://localhost:3001/api/v1/user/profile',
+                headers: { Authorization: `Bearer ${data.token}` },
+                data: data.userNames,
+            })
+            return res.data.body
+        } catch (error) {
+            if (!error.response) {
+                return rejectWithValue('ERR_NETWORK')
+            }
+            return rejectWithValue(error.response.data.message)
+        }
     }
 )
 
@@ -66,10 +81,10 @@ export const userSlice = createSlice({
                 state.error = null
             })
             .addCase(fetchUserData.rejected, (state, action) => {
-                if (action.payload === 'ERR_NETWORK') {
-                    state.error = 'Le serveur est inaccessible. Veuillez vérifier votre connexion réseau.';
+                if (action.error.message === 'ERR_NETWORK') {
+                    state.error = 'Le serveur est inaccessible. Veuillez vérifier votre connexion réseau.'
                 } else {
-                    state.error = 'Une erreur s\'est produite lors de la récupération des données utilisateur.';
+                    state.error = action.payload || 'Une erreur s\'est produite lors de la récupération des données utilisateur.'
                 }
             })
             .addCase(updateUserData.fulfilled, (state, { payload }) => {
@@ -79,10 +94,10 @@ export const userSlice = createSlice({
                 state.error = null
             })
             .addCase(updateUserData.rejected, (state, action) => {
-                if (action.payload === 'ERR_NETWORK') {
-                    state.error = 'Le serveur est inaccessible. Impossible de mettre à jour les données utilisateur.';
+                if (action.error.message === 'ERR_NETWORK') {
+                    state.error = 'Le serveur est inaccessible. Impossible de mettre à jour les données utilisateur.'
                 } else {
-                    state.error = 'Une erreur s\'est produite lors de la mise à jour des données utilisateur.';
+                    state.error = action.payload || 'Une erreur s\'est produite lors de la mise à jour des données utilisateur.'
                 }
             })
     },
